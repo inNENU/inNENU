b12e7464607a3197de2db8ea1fc21042339675f1
Mr.Hope <mister-hope@outlook.com>
2023-06-21 00:53:06 +0800

    feat: add app api

---
+++ app/function/select/api.app.ts
@@ -0,0 +1,289 @@
+import { logger } from "@mptool/enhance";
+
+import { service } from "../../utils/config.js";
+
+export interface SelectBaseOptions {
+  /**
+   * Cookie
+   */
+  cookies: string[];
+  /**
+   * 服务器地址
+   */
+  server: string;
+}
+
+export interface SelectLoginSuccessResponse {
+  status: "success";
+  cookie: string;
+  server: string;
+}
+
+export interface SelectLoginOptions {
+  id: number;
+  password: string;
+}
+
+export interface SelectLoginSuccessResponse {
+  status: "success";
+  cookies: string[];
+  server: string;
+}
+
+export interface SelectLoginFailedResponse {
+  status: "failed";
+  msg: string;
+}
+
+type SelectLoginResponse =
+  | SelectLoginSuccessResponse
+  | SelectLoginFailedResponse;
+
+export const login = (
+  options: SelectLoginOptions
+): Promise<SelectLoginResponse> =>
+  new Promise((resolve, reject) => {
+    wx.request<SelectLoginResponse>({
+      method: "POST",
+      url: `${service}select/login`,
+      data: options,
+      success: ({ data, statusCode }) => {
+        if (statusCode === 200) {
+          resolve(data);
+          if (data.status === "failed")
+            logger.error("登录失败", options, data.msg);
+        } else reject();
+      },
+      fail: () => reject(),
+    });
+  });
+
+export interface CourseData {
+  cid: string;
+  name: string;
+}
+
+export interface CourseInfo {
+  /** 名称 */
+  name: string;
+  /** 开课单位 */
+  office: string;
+  /** 类别 */
+  type: string;
+  /** 学分 */
+  point: string;
+  /** 容量 */
+  capacity: string;
+  /** 任课教师 */
+  teacher: string;
+  /** 上课周次 */
+  week: string;
+  /** 上课时间 */
+  time: string;
+  /** 上课地点 */
+  place: string;
+  /** 课 ID */
+  cid: string;
+  /** 课程 ID */
+  id: string;
+
+  /** 考试时间 */
+  examTime: string;
+  /** 周次类型 */
+  weekType: string;
+  /** 班级名称 */
+  className: string;
+}
+
+export interface MajorInfo {
+  /** 名称 */
+  name: string;
+  /** 编号 */
+  id: string;
+}
+
+export interface SelectInfoSuccessResponse {
+  status: "success";
+  jx0502id: string;
+  jx0502zbid: string;
+  courses: CourseInfo[];
+  courseTable: CourseData[][][];
+  courseTypes: string[];
+  courseOffices: string[];
+  currentMajor: string;
+  currentGrade: string;
+  grades: string[];
+  majors: MajorInfo[];
+}
+
+export interface SelectInfoFailedResponse {
+  status: "failed";
+  msg: string;
+}
+
+export type SelectInfoResponse =
+  | SelectInfoSuccessResponse
+  | SelectInfoFailedResponse;
+
+export const getInfo = (
+  options: SelectBaseOptions
+): Promise<SelectInfoResponse> =>
+  new Promise((resolve, reject) => {
+    wx.request<SelectInfoResponse>({
+      method: "POST",
+      url: `${service}select/info`,
+      data: options,
+      success: ({ data, statusCode }) => {
+        if (statusCode === 200) {
+          resolve(data);
+          if (data.status === "failed")
+            logger.error("获取信息失败", options, data.msg);
+        } else reject();
+      },
+      fail: () => reject(),
+    });
+  });
+
+export interface ProcessOptions extends SelectBaseOptions {
+  /** 课程号 */
+  id: string;
+  jx0502id: string;
+  jx0502zbid: string;
+}
+
+export interface ProcessSuccessResponse {
+  status: "success";
+  msg: string;
+}
+
+export interface ProcessFailedResponse {
+  status: "failed";
+  msg: string;
+  type?: "conflict" | "relogin";
+}
+
+export type ProcessResponse = ProcessSuccessResponse | ProcessFailedResponse;
+
+export const process = (
+  type: "add" | "delete",
+  options: ProcessOptions
+): Promise<ProcessResponse> =>
+  new Promise((resolve, reject) => {
+    wx.request<ProcessResponse>({
+      method: type === "delete" ? "DELETE" : "PUT",
+      url: `${service}select/process`,
+      data: options,
+      success: ({ data, statusCode }) => {
+        if (statusCode === 200) {
+          resolve(data);
+          if (data.status === "failed")
+            logger.error("处理选课失败", type, options, data.msg);
+        } else reject();
+      },
+      fail: () => reject(),
+    });
+  });
+
+export interface SearchOptions extends SelectBaseOptions {
+  /** 年级 */
+  grade?: string;
+  /** 专业 */
+  major?: string;
+  /** 课程类型 */
+  courseType?: string;
+  /** 课程名称 */
+  courseName?: string;
+  /** 开课单位 */
+  office?: string;
+  /** 周几 */
+  week?: string;
+  /** 节次 */
+  index?: number;
+  jx0502id: string;
+}
+
+export interface CourseBasicInfo {
+  /** 课程号 */
+  id: string;
+  /** 课程名称 */
+  name: string;
+  /** 开课单位 */
+  office: string;
+  /** 课程类型 */
+  type: string;
+}
+
+export interface SearchSuccessResponse {
+  status: "success";
+  courses: CourseBasicInfo[];
+}
+
+export interface SearchFailedResponse {
+  status: "failed";
+  msg: string;
+}
+
+export type SearchResponse = SearchSuccessResponse | SearchFailedResponse;
+
+export const search = (options: SearchOptions): Promise<SearchResponse> =>
+  new Promise((resolve, reject) => {
+    wx.request<SearchResponse>({
+      method: "POST",
+      url: `${service}select/search`,
+      data: options,
+      success: ({ data, statusCode }) => {
+        if (statusCode === 200) {
+          resolve(data);
+          if (data.status === "failed")
+            logger.error("搜索课程失败", options, data.msg);
+        } else reject();
+      },
+      fail: () => reject(),
+    });
+  });
+
+export interface StudentAmountOptions extends SelectBaseOptions {
+  /** 课程号 */
+  id: string;
+  jx0502id: string;
+}
+
+export interface StudentAmountData {
+  /** 课程号 */
+  cid: string;
+  /** 选课人数 */
+  amount: number;
+}
+
+export interface StudentAmountSuccessResponse {
+  status: "success";
+  data: StudentAmountData[];
+}
+
+export interface StudentAmountFailedResponse {
+  status: "failed";
+  msg: string;
+}
+
+export type StudentAmountResponse =
+  | StudentAmountSuccessResponse
+  | StudentAmountFailedResponse;
+
+export const getAmount = (
+  options: StudentAmountOptions
+): Promise<StudentAmountResponse> =>
+  new Promise((resolve, reject) => {
+    wx.request<StudentAmountResponse>({
+      method: "POST",
+      url: `${service}select/student-amount`,
+      data: options,
+      success: ({ data, statusCode }) => {
+        if (statusCode === 200) {
+          resolve(data);
+          if (data.status === "failed")
+            logger.error("获取选课人数失败", options, data.msg);
+        } else reject();
+      },
+      fail: () => reject(),
+    });
+  });
