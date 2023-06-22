da4815c82a275e41f016cc5c0979db70ee34dae1
Mr.Hope <mister-hope@outlook.com>
2023-06-23 00:14:51 +0800

    chore: tweaks

--- app/utils/config.ts
+++ app/utils/config.ts
@@ -1,6 +1,7 @@
 /** 小程序服务器地址 */
 export const assets = "https://assets.innenu.com/";
 export const server = "https://mp.innenu.com/";
+// export const service = "http://localhost:8080/";
 export const service = "https://service.innenu.com/";
 
 /** 小程序版本 */
