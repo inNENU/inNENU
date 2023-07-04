bd82714f5b1673378b924c14f0d2fd010468460b
Mr.Hope <mister-hope@outlook.com>
2023-07-04 12:43:19 +0800

    fix: fix theme picker

--- app/pages/user/user.ts
+++ app/pages/user/user.ts
@@ -110,7 +110,7 @@ $Page("user", {
     const theme = (
       (
         (this.data.page.content[1] as FunctionalListComponentConfig)
-          .items[0] as PickerListComponentItemConfig
+          .items[1] as PickerListComponentItemConfig
       ).select as string[]
     )[Number(value)];
 
