7087433ec27da62287752439d4f3c9ca5d6870cb
Mr.Hope <mister-hope@outlook.com>
2023-07-10 19:12:59 +0800

    chore: update course api

--- app/function/course/api.ts
+++ app/function/course/api.ts
@@ -1,24 +1,22 @@
 import { logger } from "@mptool/enhance";
 import { get, set } from "@mptool/file";
 
-import { type Cookie, type CookieOptions } from "../../../typings/cookie.js";
-import { type CommonFailedResponse } from "../../../typings/response.js";
+import {
+  type UnderSystemLoginResponse,
+  type UserCourseTableOptions,
+  type UserCourseTableResponse,
+  type UserGradeListOptions,
+  type UserGradeListResponse,
+} from "./typings.js";
+import {
+  type Cookie,
+  type CookieVerifyResponse,
+} from "../../../typings/index.js";
 import { request } from "../../api/net.js";
 import { service } from "../../config/info.js";
 import { UNDER_SYSTEM_COOKIE } from "../../config/keys.js";
-import { LoginFailedResponse } from "../../utils/account.js";
 import { type AccountBasicInfo } from "../../utils/app.js";
 import { HOUR } from "../../utils/constant.js";
-
-export interface UnderSystemLoginSuccessResponse {
-  status: "success";
-
-  cookies: Cookie[];
-}
-
-export type UnderSystemLoginResponse =
-  | UnderSystemLoginSuccessResponse
-  | LoginFailedResponse;
 
 const getCookie = (
   options: AccountBasicInfo,
@@ -33,15 +31,6 @@ const getCookie = (
 
     return data;
   });
-
-export interface CookieVerifySuccessResponse {
-  status: "success";
-  valid: boolean;
-}
-
-export type CookieVerifyResponse =
-  | CookieVerifySuccessResponse
-  | CommonFailedResponse;
 
 export const check = (cookies: Cookie[]): Promise<CookieVerifyResponse> =>
   request<CookieVerifyResponse>(`${service}under-system/check`, {
@@ -61,39 +50,6 @@ export const login = (
     : getCookie(account);
 };
 
-interface UserCourseTableExtraOptions {
-  /** 学号 */
-  id: number;
-  /** 查询时间 */
-  time: string;
-}
-
-export type UserCourseTableOptions = (AccountBasicInfo | CookieOptions) &
-  UserCourseTableExtraOptions;
-
-export interface ClassItem {
-  name: string;
-  teacher: string;
-  time: string;
-  location: string;
-}
-
-export type CellItem = ClassItem[];
-export type RowItem = CellItem[];
-export type TableItem = RowItem[];
-
-export interface UserCourseTableSuccessResponse {
-  status: "success";
-  data: TableItem;
-  startTime: string;
-}
-
-export type UserCourseTableFailedResponse = LoginFailedResponse;
-
-export type UserCourseTableResponse =
-  | UserCourseTableSuccessResponse
-  | UserCourseTableFailedResponse;
-
 export const getCourseTable = (
   options: UserCourseTableOptions,
 ): Promise<UserCourseTableResponse> =>
@@ -105,75 +61,6 @@ export const getCourseTable = (
 
     return data;
   });
-
-type CourseType =
-  | "通识教育必修课"
-  | "通识教育选修课"
-  | "专业教育必修课"
-  | "专业教育选修课"
-  | "教师职业教育必修课"
-  | "教师职业教育选修课"
-  | "任意选修课"
-  | "发展方向课"
-  | "教师教育必修课"
-  | "教师教育选修课";
-
-export interface UserGradeListExtraOptions {
-  /** 查询时间 */
-  time?: string;
-  /** 课程名称 */
-  name?: string;
-  /** 课程性质 */
-  courseType?: CourseType;
-  gradeType?: "all" | "best";
-}
-
-export type UserGradeListOptions = (AccountBasicInfo | CookieOptions) &
-  UserGradeListExtraOptions;
-
-export interface GradeResult {
-  /** 修读时间 */
-  time: string;
-  /** 课程 id */
-  cid: string;
-  /** 课程名称 */
-  name: string;
-  /** 难度系数 */
-  difficulty: number;
-  /** 分数 */
-  grade: number;
-  /** 绩点成绩 */
-  gradePoint: number;
-  /** 成绩标志 */
-  mark: string;
-  /** 课程类型 */
-  courseType: string;
-  /** 选修课类型 */
-  commonType: string;
-  /** 课程类型短称 */
-  shortCourseType: string;
-  /** 学时 */
-  hours: number | null;
-  /** 学分 */
-  point: number;
-  /** 考试性质 */
-  examType: string;
-  /** 补重学期 */
-  reLearn: string;
-  /** 审核状态 */
-  status: string;
-}
-
-export interface UserGradeListSuccessResponse {
-  status: "success";
-  data: GradeResult[];
-}
-
-export type UserGradeListFailedResponse = LoginFailedResponse;
-
-export type UserGradeListResponse =
-  | UserGradeListSuccessResponse
-  | UserGradeListFailedResponse;
 
 export const getGradeList = (
   options: UserGradeListOptions,
---
+++ app/function/course/typings.d.ts
@@ -0,0 +1,115 @@
+import { type Cookie, type CookieOptions } from "../../../typings/cookie.js";
+import { LoginFailedResponse } from "../../utils/account.ts";
+import { type AccountBasicInfo } from "../../utils/app.ts";
+
+export interface UnderSystemLoginSuccessResponse {
+  status: "success";
+
+  cookies: Cookie[];
+}
+
+export type UnderSystemLoginResponse =
+  | UnderSystemLoginSuccessResponse
+  | LoginFailedResponse;
+
+interface UserCourseTableExtraOptions {
+  /** 学号 */
+  id: number;
+  /** 查询时间 */
+  time: string;
+}
+
+export type UserCourseTableOptions = (AccountBasicInfo | CookieOptions) &
+  UserCourseTableExtraOptions;
+
+export interface ClassItem {
+  name: string;
+  teacher: string;
+  time: string;
+  location: string;
+}
+
+export type CellItem = ClassItem[];
+export type RowItem = CellItem[];
+export type TableItem = RowItem[];
+
+export interface UserCourseTableSuccessResponse {
+  status: "success";
+  data: TableItem;
+  startTime: string;
+}
+
+export type UserCourseTableFailedResponse = LoginFailedResponse;
+
+export type UserCourseTableResponse =
+  | UserCourseTableSuccessResponse
+  | UserCourseTableFailedResponse;
+
+export type CourseType =
+  | "通识教育必修课"
+  | "通识教育选修课"
+  | "专业教育必修课"
+  | "专业教育选修课"
+  | "教师职业教育必修课"
+  | "教师职业教育选修课"
+  | "任意选修课"
+  | "发展方向课"
+  | "教师教育必修课"
+  | "教师教育选修课";
+
+export interface UserGradeListExtraOptions {
+  /** 查询时间 */
+  time?: string;
+  /** 课程名称 */
+  name?: string;
+  /** 课程性质 */
+  courseType?: CourseType;
+  gradeType?: "all" | "best";
+}
+
+export type UserGradeListOptions = (AccountBasicInfo | CookieOptions) &
+  UserGradeListExtraOptions;
+
+export interface GradeResult {
+  /** 修读时间 */
+  time: string;
+  /** 课程 id */
+  cid: string;
+  /** 课程名称 */
+  name: string;
+  /** 难度系数 */
+  difficulty: number;
+  /** 分数 */
+  grade: number;
+  /** 绩点成绩 */
+  gradePoint: number;
+  /** 成绩标志 */
+  mark: string;
+  /** 课程类型 */
+  courseType: string;
+  /** 选修课类型 */
+  commonType: string;
+  /** 课程类型短称 */
+  shortCourseType: string;
+  /** 学时 */
+  hours: number | null;
+  /** 学分 */
+  point: number;
+  /** 考试性质 */
+  examType: string;
+  /** 补重学期 */
+  reLearn: string;
+  /** 审核状态 */
+  status: string;
+}
+
+export interface UserGradeListSuccessResponse {
+  status: "success";
+  data: GradeResult[];
+}
+
+export type UserGradeListFailedResponse = LoginFailedResponse;
+
+export type UserGradeListResponse =
+  | UserGradeListSuccessResponse
+  | UserGradeListFailedResponse;
