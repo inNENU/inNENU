fd90b9c10bf4e2f5ce142c0f9725220b78827f6c
Mr.Hope <mister-hope@outlook.com>
2023-07-14 00:48:22 +0800

    chore: back to 5.8.0

--- app/config/info.qy.ts
+++ app/config/info.qy.ts
@@ -1,7 +1,7 @@
 /** 小程序名称 */
 export const appName = "myNENU";
 /** 小程序版本 */
-export const version = "5.7.0";
+export const version = "5.8.0";
 /** 小程序服务器地址 */
 export const assets = "https://assets.innenu.com/";
 export const server = "https://mp.innenu.com/";
