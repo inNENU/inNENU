63aec9831216ddc60ac8c63111be0f5f35d5bfe9
Mr.Hope <mister-hope@outlook.com>
2023-07-27 23:33:18 +0800

    chore: bump version

--- app/config/info.qy.ts
+++ app/config/info.qy.ts
@@ -1,7 +1,7 @@
 /** 小程序名称 */
 export const appName = "myNENU";
 /** 小程序版本 */
-export const version = "6.0.0";
+export const version = "6.0.1";
 /** 小程序服务器地址 */
 export const assets = "https://assets.innenu.com/";
 export const server = "https://mp.innenu.com/";
