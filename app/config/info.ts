b41fe5cc96d332c6d7c792ce7f0cdc25e3c055ed
Mr.Hope <mister-hope@outlook.com>
2023-08-01 00:08:44 +0800

    chore: fix api

--- app/config/info.ts
+++ app/config/info.ts
@@ -9,4 +9,4 @@ export const service = "https://service.innenu.com/";
 // export const service = "http://localhost:8080/";
 export const appCoverPrefix = `${server}img/inNENU`;
 export const imageWaterMark =
-  "?x-oss-process=image/watermark,image_aW1nL2luTkVOVS5wbmc_eC1vc3MtcHJvY2Vzcz1pbWFnZS9yZXNpemUsUF80,text_aW7kuJzluIjvvIzlsLHnlKhpbuS4nOW4iA==,type_ZmFuZ3poZW5nc2h1c29uZw,align_1,interval_8,size_16";
+  "?x-oss-process=image/watermark,text_aW7kuJzluIjvvIzlsLHnlKg,image_aW1nL2luTkVOVS5wbmc_eC1vc3MtcHJvY2Vzcz1pbWFnZS9yZXNpemUsUF80,type_ZmFuZ3poZW5nc2h1c29uZw,order_1,align_1,interval_8,size_16";
