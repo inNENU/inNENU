93a6c498ba4cf5547925e22c5280582a15b866ea
Mr.Hope <mister-hope@outlook.com>
2023-09-22 12:38:59 +0800

    chore: update key name

--- app/config/keys.ts
+++ app/config/keys.ts
@@ -6,7 +6,7 @@ export const USER_INFO_KEY = "nenu-user-info-v3";
 
 export const BORROW_BOOKS_KEY = "borrow-books-info";
 export const CARD_BALANCE_KEY = "card-balance-info";
-export const COURSE_DATA_KEY = "course-data-info";
+export const COURSE_DATA_KEY = "course-data-info-v2";
 export const CHANGE_MAJOR_DATA_KEY = "change-major-data-info";
 export const EXAM_PLACE_DATA_KEY = "exam-place-data-info";
 export const EMAIL_DATA_KEY = "email-data-info";
