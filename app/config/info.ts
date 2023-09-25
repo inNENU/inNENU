fd4f0b7df8a777b48b13fab65535785880181461
Mr.Hope <mister-hope@outlook.com>
2023-09-25 18:48:09 +0800

    chore: bump version

--- app/config/info.ts
+++ app/config/info.ts
@@ -1,7 +1,7 @@
 /** 小程序名称 */
 export const appName = "inNENU";
 /** 小程序版本 */
-export const version = "6.6.1";
+export const version = "6.6.2";
 /** 小程序服务器地址 */
 export const assets = "https://assets.innenu.com/";
 export const server = "https://mp.innenu.com/";
