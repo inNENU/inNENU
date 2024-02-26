5954eb750775f7fb45129019e938985e7df2f0a7
Mr.Hope <mister-hope@outlook.com>
2024-02-26 10:50:14 +0800

    chore: update course key

--- app/config/keys.ts
+++ app/config/keys.ts
@@ -4,7 +4,7 @@ export const PRIVACY_KEY = "privacy-agreement";
 
 export const BORROW_BOOKS_KEY = "borrow-books-info";
 export const CARD_BALANCE_KEY = "card-balance-info";
-export const COURSE_DATA_KEY = "course-data-info-v2";
+export const COURSE_DATA_KEY = "course-data-info-v3";
 export const CHANGE_MAJOR_DATA_KEY = "change-major-data-info";
 export const EMAIL_DATA_KEY = "email-data-info";
 export const EXAM_PLACE_DATA_KEY = "exam-place-data-info";
