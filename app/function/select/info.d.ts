a1f0cb560c23519dc4f2c6257dc2070665142dcb
Mr.Hope <mister-hope@outlook.com>
2024-06-14 11:15:20 +0800

    chore: tweaks select

--- app/function/select/info.d.ts
+++
@@ -1,5 +0,0 @@
-import type { SelectInfoOptions, SelectInfoResponse } from "./typings";
-
-export const getInfo: (
-  options: SelectInfoOptions,
-) => Promise<SelectInfoResponse>;
--- app/function/select/search.d.ts
+++
@@ -1,3 +0,0 @@
-import type { SearchOptions, SelectSearchResponse } from "./typings.js";
-
-export const search: (options: SearchOptions) => Promise<SelectSearchResponse>;
