d55bdd9455cc2962c5e3b2e740c90ee0a8da3c39
Mr.Hope <mister-hope@outlook.com>
2024-10-18 01:58:04 +0800

    chore: tweaks

--- app/config/info.we.ts
+++ app/config/info.we.ts
@@ -6,7 +6,7 @@ export const description = "weNENU，我们的东师";
 /** 小程序版本 */
 export const version = "0.0.240902";
 
-// FIXME: Update Cover, logo and watermark
 export const appCoverPrefix = `${server}img/weNENU`;
 export const logo = "/frameset/placeholder.png";
+// FIXME: update watermark
 export const imageWaterMark = "";
