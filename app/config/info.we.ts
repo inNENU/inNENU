c4abcec73f8916b9ec7f5bb122f24ec222ff789b
Mr.Hope <mister-hope@outlook.com>
2025-03-06 18:23:16 +0800

    chore: bump version

--- app/config/info.we.ts
+++ app/config/info.we.ts
@@ -4,7 +4,7 @@ import { server } from "./server.js";
 export const appName = "weNENU";
 export const description = "weNENU，我们的东师";
 /** 小程序版本 */
-export const version = "0.0.250108";
+export const version = "0.0.250306";
 
 export const appCoverPrefix = `${server}img/weNENU`;
 export const logo = "/frameset/placeholder.png";
