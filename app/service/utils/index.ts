b8d15a93b2be78674898429b49684ee76b946422
Mr.Hope <mister-hope@outlook.com>
2025-03-07 15:30:53 +0800

    chore: tweaks

--- app/service/utils/index.ts
+++ app/service/utils/index.ts
@@ -1,5 +1,5 @@
-export * from "./fail.js";
 export * from "./actionFailType.js";
+export * from "./fail.js";
 export * from "./response.js";
 export * from "./utils.js";
 export type LoginMethod = "check" | "validate" | "force";
