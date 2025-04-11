03821b97107949257bef692e8387ee1739fc6983
Mister-Hope <mister-hope@outlook.com>
2025-04-11 23:31:00 +0800

    feat: remove framesaet

--- app/config/info.ts
+++ app/config/info.ts
@@ -1,7 +1,4 @@
-import { server } from "./server.js";
-
-// @ts-expect-error: DEBUG is not standard
-wx.env.DEBUG = true;
+import { assets, server } from "./server.js";
 
 /** 小程序名称 */
 export const appName = "inNENU";
@@ -10,6 +7,6 @@ export const description = "在东师，就用 inNENU";
 export const version = "7.3.0";
 
 export const appCoverPrefix = `${server}img/inNENU`;
-export const logo = "/frameset/placeholder.png";
+export const logo = `${assets}img/inNENU.png`;
 export const imageWaterMark =
   "?x-oss-process=image/watermark,text_aW7kuJzluIjvvIzlsLHnlKg,image_aW1nL2luTkVOVS5wbmc_eC1vc3MtcHJvY2Vzcz1pbWFnZS9yZXNpemUsUF80,type_ZmFuZ3poZW5nc2h1c29uZw,order_1,align_1,interval_8,size_16";
--- app/config/info.we.ts
+++ app/config/info.we.ts
@@ -1,4 +1,4 @@
-import { server } from "./server.js";
+import { assets, server } from "./server.js";
 
 /** 小程序名称 */
 export const appName = "weNENU";
@@ -7,6 +7,7 @@ export const description = "weNENU，我们的东师";
 export const version = "0.0.250306";
 
 export const appCoverPrefix = `${server}img/weNENU`;
-export const logo = "/frameset/placeholder.png";
+export const logo = `${assets}img/weNENU.png`;
 // FIXME: update watermark
-export const imageWaterMark = "";
+export const imageWaterMark =
+  "?x-oss-process=image/watermark,image_aW1nL3dlTkVOVS5wbmc_eC1vc3MtcHJvY2Vzcz1pbWFnZS9yZXNpemUsUF80,text_5oiR5Lus55qEIE5FTlU,type_ZmFuZ3poZW5nc2h1c29uZw,order_0,align_1,interval_8,size_16";
