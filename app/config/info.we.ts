4a13458f28af8d008a7bfc4ab16e2bb7d3e8cdda
Mister-Hope <mister-hope@outlook.com>
2025-04-11 23:45:50 +0800

    feat: bump sdk

--- app/config/info.we.ts
+++ app/config/info.we.ts
@@ -8,6 +8,5 @@ export const version = "0.0.250306";
 
 export const appCoverPrefix = `${server}img/weNENU`;
 export const logo = `${assets}img/weNENU.png`;
-// FIXME: update watermark
 export const imageWaterMark =
   "?x-oss-process=image/watermark,image_aW1nL3dlTkVOVS5wbmc_eC1vc3MtcHJvY2Vzcz1pbWFnZS9yZXNpemUsUF80,text_5oiR5Lus55qEIE5FTlU,type_ZmFuZ3poZW5nc2h1c29uZw,order_0,align_1,interval_8,size_16";
