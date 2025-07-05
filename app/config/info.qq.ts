3d9e402c87bc19a90f8227d604a54a048db07920
Mister-Hope <mister-hope@outlook.com>
2025-07-05 14:42:37 +0800

    fix: fix share image

--- app/config/info.qq.ts
+++ app/config/info.qq.ts
@@ -1,4 +1,4 @@
-import { assets, server } from "./server.js";
+import { assets } from "./server.js";
 
 /** 小程序名称 */
 export const appName = "inNENU";
@@ -6,7 +6,7 @@ export const description = "在东师，就用 inNENU";
 /** 小程序版本 */
 export const version = "7.3.1";
 
-export const appCoverPrefix = `${server}img/inNENU`;
+export const appCoverPrefix = `${assets}img/inNENU`;
 export const logo = `${assets}img/inNENU.png`;
 export const imageWaterMark =
   "?x-oss-process=image/watermark,text_5Zyo5Lic5biI77yM5bCx55So,image_aW1nL2luTkVOVS5wbmc_eC1vc3MtcHJvY2Vzcz1pbWFnZS9yZXNpemUsUF80,type_ZmFuZ3poZW5nc2h1c29uZw,order_1,align_1,interval_8,size_16";
--- app/config/info.we.ts
+++ app/config/info.we.ts
@@ -1,4 +1,4 @@
-import { assets, server } from "./server.js";
+import { assets } from "./server.js";
 
 /** 小程序名称 */
 export const appName = "weNENU";
@@ -6,7 +6,7 @@ export const description = "weNENU，我们的东师";
 /** 小程序版本 */
 export const version = "0.0.250627";
 
-export const appCoverPrefix = `${server}img/weNENU`;
+export const appCoverPrefix = `${assets}img/weNENU`;
 export const logo = `${assets}img/weNENU.png`;
 export const imageWaterMark =
   "?x-oss-process=image/watermark,image_aW1nL3dlTkVOVS5wbmc_eC1vc3MtcHJvY2Vzcz1pbWFnZS9yZXNpemUsUF80,text_5oiR5Lus55qEIE5FTlU,type_ZmFuZ3poZW5nc2h1c29uZw,order_0,align_1,interval_8,size_16";
